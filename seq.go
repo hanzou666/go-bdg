bea8c55ecb93e65e6b537b4f7bae7dd299eed284
Shuho Ohwada <hanzou6326@gmail.com>
2020-05-31 21:02:59 +0900

    add node

---
+++ seq.go
@@ -0,0 +1,22 @@
+package bdg
+
+type DnaSeq struct {
+	Seq []byte
+}
+
+type Coordinate struct {
+	Start int64
+	End   int64
+}
+
+func NewDnaSeq(dnaStr string) *DnaSeq {
+	return &DnaSeq{Seq: []byte(dnaStr)}
+}
+
+func (d DnaSeq) MarshalText() ([]byte, error) {
+	return d.Seq, nil
+}
+
+func (d *DnaSeq) Len() int64 {
+	return int64(len(d.Seq))
+}
